bf1cf7895aa23aaf9cceb93647e81f0066ea93d7
Romeu <passadore.neto@outlook.com>
2024-07-24 16:31:58 -0300

    Renderizando condicionalmente

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -13,12 +13,16 @@ export default {
             <span class="subtitulo-lg sua-lista-texto">
                 Sua lista:
             </span>
-            <ul class="ingredientes-sua-lista">
-                <li v-for="ingrediente in ingredientes" class="ingrediente">
+            <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
+                <li v-for="ingrediente in ingredientes" :key="ingrediente" class="ingrediente">
                     {{ ingrediente }}
                 </li>
 
             </ul>
+            <p v-else class="paragrafo lista-vazia">
+                <img src="../assets/icones/lista-vazia.svg" alt="icone de pesquisa">
+                 Sua lista está vazia, selecione ingredientes para iniciar
+            </p>
         </section>
 
     </main>
