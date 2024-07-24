440491e5436b2430ad22b48a8ed010bb26dd080b
Romeu <passadore.neto@outlook.com>
2024-07-24 15:34:10 -0300

    Repetindo itens com v-for

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <script lang="ts">
 import Banner from './components/Banner.vue';
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
 export default {
-  components:{Banner: Banner}
+  components:{Banner: Banner,ConteudoPrincipal:ConteudoPrincipal}
 }
 </script>
 <template>
   <Banner />
+  <ConteudoPrincipal />
+
+  
   
 </template>
 <style scoped>
 
+
+   
 </style>
