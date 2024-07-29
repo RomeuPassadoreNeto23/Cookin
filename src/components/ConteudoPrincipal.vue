33958043fe7405ed2916424d0a0c379bb8530969
Romeu <passadore.neto@outlook.com>
2024-07-29 15:27:25 -0300

    Importando dados

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -1,10 +1,13 @@
 <script lang="ts">
+import SelecionarIngredientes from './SelecionarIngredientes.vue';
+
 export default {
     data() {
         return {
             ingredientes: ['Alho', 'Manteiga', 'Orégano']
-        }
-    }
+        };
+    },
+    components:{SelecionarIngredientes:SelecionarIngredientes}
 }
 </script>
 <template>
@@ -24,6 +27,7 @@ export default {
                  Sua lista está vazia, selecione ingredientes para iniciar
             </p>
         </section>
+        <SelecionarIngredientes />
 
     </main>
 </template>
