3d9e2c521933acbca07e03df3fa77c376ce6eedf
Romeu <passadore.neto@outlook.com>
2024-07-30 14:10:19 -0300

    Re-renderizando a tela com estado

--- src/components/SelecionarIngredientes.vue
+++ src/components/SelecionarIngredientes.vue
@@ -1,13 +1,18 @@
 <script lang="ts">
 import {obterCategorias} from "@/http/index";
+import type ICategoria from "@/interfaces/ICategoria"
 export default {
     
     data() {
         return {
-            categorias:obterCategorias()
+            categorias:[] as ICategoria[]
 
         }
-    }
+    },
+   async created() {
+   this.categorias = await obterCategorias();
+        
+    },
 
 }
 </script>
