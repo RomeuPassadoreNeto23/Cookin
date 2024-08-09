c17e6304f1f036c98c7b55cfdb2dc04d38b06500
Romeu Passadore Neto <passadore.neto@outlook.com>
2024-08-09 18:00:22 -0300

    Importando imagens dinamicamente

--- src/components/SelecionarIngredientes.vue
+++ src/components/SelecionarIngredientes.vue
@@ -1,18 +1,21 @@
 <script lang="ts">
-import {obterCategorias} from "@/http/index";
-import type ICategoria from "@/interfaces/ICategoria"
+import { obterCategorias } from "@/http/index";
+import type ICategoria from "@/interfaces/ICategoria";
+import CardCategoria from "./CardCategoria.vue";
+
 export default {
-    
+
     data() {
         return {
-            categorias:[] as ICategoria[]
+            categorias: [] as ICategoria[]
 
         }
     },
-   async created() {
-   this.categorias = await obterCategorias();
-        
+    async created() {
+        this.categorias = await obterCategorias();
+
     },
+    components: { CardCategoria }
 
 }
 </script>
@@ -25,7 +28,7 @@ export default {
         </p>
         <ul class="categorias">
             <li v-for="categoria in categorias" :key="categoria.nome">
-                {{ categoria.nome }}
+                <CardCategoria  :categoria="categoria"/>
 
             </li>
 
