138ba3934e1f01c5f40366ff4b5fb0b718715261
Romeu Passadore Neto <passadore.neto@outlook.com>
2024-08-10 11:46:43 -0300

    Personalizando estilos

--- src/components/Tag.vue
+++ src/components/Tag.vue
@@ -1,7 +1,8 @@
 <script lang="ts">
 export default {
     props: {
-        texto: { type: String, required: true }
+        texto: { type: String, required: true },
+        ativa: Boolean
 
     }
 }
@@ -9,7 +10,7 @@ export default {
 
 
 <template>
-    <span class="tag">
+    <span :class="['tag', { ativa }]">
         {{ texto }}
     </span>
 </template>
@@ -23,6 +24,12 @@ export default {
     padding: 0.5rem;
     text-align: center;
     transition: 0.2s;
+    color: var(--cinza);
+    background: var(--cinza-claro);
+    font-weight: 400;
+}
+
+.tag.ativa {
     color: var(--creme, #FFFAF3);
     background: var(--coral, #F0633C);
     font-weight: 700;
