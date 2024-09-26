4818c4741df84e6631195eb072be893aeecb4fad
Romeu <passadore.neto@outlook.com>
2024-09-26 17:45:59 -0300

    começando CardReceita e MostrarReceitas

---
+++ src/components/CardReceita.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import type IReceiras from '@/interfaces/IReceitas';
+import type { PropType } from 'vue';
+export default {
+    props: {
+        receitas:{type: Object as PropType<IReceiras>, required: true }
+
+    }
+}
+
+</script>
+<template>
+    <article class="card-receita">
+        <img :src="`/public/imagens/receitas/${receitas.imagem}`" >
+        <h1><strong>{{ receitas.nome }}</strong></h1>
+    </article>
+
+</template>
--- src/components/MostrarReceitas.vue
+++ src/components/MostrarReceitas.vue
@@ -1,6 +1,7 @@
 <script lang="ts">
 import { obterReceitas } from '@/http';
 import type IReceiras from '@/interfaces/IReceitas';
+import CardReceita from './CardReceita.vue';
 export default {
     data() {
 
@@ -14,10 +15,52 @@ export default {
         this.receitas = await obterReceitas();
 
     },
+    components: { CardReceita }
 }
 
 
 </script>
 <template>
- Mostrar Receita 
+    <section class="mostrar-receita">
+        <h1 class="cabecalho titulo-receita">Receitas</h1>
+        <p class="resultado paragrafo-lg">Resultados encontrados: {{ receitas.length }}</p>
+        <p class=" paragrafo-lg descricao">Veja as opções de receitas que encontramos com os ingredientes que você tem
+            por aí!</p>
+        <ul class="receita">
+            <li v-for="receita in receitas" :key="receita.nome">
+                <CardReceita :receitas="receita" />
+            </li>
+        </ul>
+    </section>
 </template>
+<style scoped>
+.mostrar-receita {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.titulo-receita {
+    color: var(--verde-medio, #3D6D4A);
+    display: block;
+    margin-bottom: 1.5rem;
+
+}
+
+.resultado {
+    color: var(--verde-medio, #3D6D4A);
+
+}
+
+.descricao {
+    margin-bottom: 2rem;
+}
+
+.receita {
+    margin-bottom: 1rem;
+    display: flex;
+    justify-content: center;
+    gap: 1.5rem;
+    flex-wrap: wrap;
+}
+</style>
