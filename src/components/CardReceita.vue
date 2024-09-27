d560f1d866c534d741d29c12fbee57c3ff3d86b2
Romeu <passadore.neto@outlook.com>
2024-09-27 12:03:04 -0300

    CSS do CardReceita feito

--- src/components/CardReceita.vue
+++ src/components/CardReceita.vue
@@ -3,7 +3,7 @@ import type IReceiras from '@/interfaces/IReceitas';
 import type { PropType } from 'vue';
 export default {
     props: {
-        receitas:{type: Object as PropType<IReceiras>, required: true }
+        receitas: { type: Object as PropType<IReceiras>, required: true }
 
     }
 }
@@ -11,8 +11,40 @@ export default {
 </script>
 <template>
     <article class="card-receita">
-        <img :src="`/public/imagens/receitas/${receitas.imagem}`" >
-        <h1><strong>{{ receitas.nome }}</strong></h1>
+        <img :src="`/public/imagens/receitas/${receitas.imagem}`">
+        <h1>{{ receitas.nome }}</h1>
     </article>
 
 </template>
+<style scoped>
+.card-receita {
+    display: flex;
+    justify-content:flex-start;
+    align-items:center;
+    flex-direction: column;
+    width:282px;
+    min-height:246px; 
+    max-width: 312px;
+    border-radius: 16px;
+    box-shadow: 4px 4px 12px 0px rgba(68, 68, 68, 0.08);
+    background-color: rgba(255, 255, 255, 1);
+}
+.card-receita img {
+    width: 282px;
+    height: 150px;
+    border-radius: 16px 16px 0px 0px;
+    margin-bottom: 30px;
+ 
+    
+
+}
+.card-receita h1{
+    width:282px;
+    height:22px;
+    font-size: 18px;
+    font-weight: 700;
+    font-family:Paragraph Bold, sans-serif ;
+    text-align: center;
+
+}
+</style>
